8d8980960c8ca6f0b3e03548e9ca9416fc8a6335
Mythili-Palanisamy <palanisamymythili22@gmail.com>
2024-12-18 10:02:54 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import TodoWrapper from "./Components/TodoWrapper";
+
+function App() {
+  return (
+    <>
+      <TodoWrapper />
+    </>
+  );
+}
+
+export default App;
